1544d7fcbd73b1422eefc77ea83c3cc18ee4c9b1
pvijayreddy9 <163135946+pvijayreddy9@users.noreply.github.com>
2024-03-16 08:21:12 +0530

    LinkedList

---
+++ Training/src/com/sample/training/collections/CustomLinkedList.java
@@ -0,0 +1,72 @@
+package com.sample.training.collections;
+
+public class CustomLinkedList<E> {
+    private Node<E> head;
+    private int size;
+
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public CustomLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<E> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        Node<E> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        Node<E> current = head;
+        while (current != null) {
+            sb.append(current.data);
+            if (current.next != null) {
+                sb.append(", ");
+            }
+            current = current.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    
+}
+
--- Training/src/com/sample/training/collections/DemoCollections.java
+++ Training/src/com/sample/training/collections/DemoCollections.java
@@ -1,5 +1,7 @@
 package com.sample.training.collections;
 
+import java.util.LinkedList;
+
 public class DemoCollections {
 
 	public static void main(String[] args) {
@@ -9,11 +11,22 @@ public class DemoCollections {
 		customArrayList.add(567);
 		customArrayList.add(8910);
 		customArrayList.add(101112);
-		System.out.println("Size: "+customArrayList.size());
+		//System.out.println("Size: "+customArrayList.size());
 		customArrayList.add(101113);
 		
-		System.out.println("Size: "+customArrayList.size());
-		System.out.println("ArrayList Elements: "+ customArrayList);
+		//System.out.println("Size: "+customArrayList.size());
+		//System.out.println("ArrayList Elements: "+ customArrayList);
+		
+		CustomLinkedList<String> list = new CustomLinkedList<>();
+		list.add("hi");
+		list.add("hello");
+		list.add("Good Morning");
+		list.add("to all");
+		
+		System.out.println("CustomLinkedList Elements: "+list);
+		System.out.println("CustomLinkedList Size: "+list.size());
+		System.out.println("CustomLinkedList get an element: "+list.get(3));
+		
 	}
 
 }

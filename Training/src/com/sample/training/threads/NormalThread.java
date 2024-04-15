2f88ed02f0f1868d93bc529e69d716da9ff1e96b
pvijayreddy9 <163135946+pvijayreddy9@users.noreply.github.com>
2024-04-15 20:47:05 +0530

    commit

--- Training/src/com/sample/training/threads/NormalThread.java
+++ Training/src/com/sample/training/threads/NormalThread.java
@@ -2,6 +2,8 @@ package com.sample.training.threads;
 
 public class NormalThread extends Thread {
 
+	
+
 	@Override
 	public void run() {
 		System.out.println("This is NormalThread");
--- Training/src/com/sample/training/threads/ThreadManager.java
+++ Training/src/com/sample/training/threads/ThreadManager.java
@@ -4,9 +4,11 @@ public class ThreadManager {
 
 	public static void main(String[] args) {
 		RunnableThread runnable = new RunnableThread();
+		Thread t1 = new Thread(runnable);
 		NormalThread thread = new NormalThread();
 		
-		runnable.run();
+		t1.run();
+		t1.start();
 		thread.run();
 		thread.start();
 	}
